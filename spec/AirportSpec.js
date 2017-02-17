c6c4c4caddeee4e6d9bd0cf4f8011d7bb53a3a95
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-17 15:46:30 +0000

    Adds land plane functions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,10 +3,16 @@ describe("Airport", function() {
 
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpy('plane')
   });
 
   it("should initiate the airport with an empty plane array", function() {
     expect(airport.planes).toEqual([]);
+  });
+
+  it("allows the plane to land and shows the plane is in the airport", function() {
+    airport.landPlane(plane)
+    expect(airport.planes).toEqual([plane]);
   });
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,3 +1,7 @@
 function Airport() {
   this.planes = [];
 };
+
+  Airport.prototype.landPlane = function(plane) {
+    this.planes.push(plane);
+  };
