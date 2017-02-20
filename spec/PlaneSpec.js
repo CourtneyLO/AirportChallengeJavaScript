61b0523adcbf1af37ea849f6abbcb196a779c704
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-20 10:22:50 +0000

    Finished program

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -3,7 +3,8 @@ describe("Plane", function(){
 
   beforeEach(function(){
     plane = new Plane();
-    airport = jasmine.createSpy("airport")
+    Gatwick = jasmine.createSpy("airport")
+    Heathrow = jasmine.createSpy("airport")
   });
 
   it("should be initialized as in flight", function(){
@@ -11,29 +12,30 @@ describe("Plane", function(){
   });
 
   it("should change the inFlight status of the plane when it lands", function() {
-    plane.land();
+    plane.land(Gatwick);
     expect(plane.inFlight).toEqual(false);
   });
 
   it("should change the inFlight status of the plane to when it takes off", function() {
-    plane.land();
+    plane.land(Gatwick);
     plane.takeOff();
     expect(plane.inFlight).toEqual(true);
   });
 
   it("should raise an error if the plane is in airport but land plane is called", function(){
-    plane.land();
+    plane.land(Gatwick);
     expect(function() {plane.land()}).toThrowError("Plane cannot land: Plan has already landed")
   });
 
   it("should raise an error if the plane is already in flight but take off is called", function() {
-    plane.land();
+    plane.land(Gatwick);
     plane.takeOff();
     expect(function() {plane.takeOff()}).toThrowError("Plane cannot take off: The plane is already in the air")
   });
 
   it("should indictae which airport a plane is when it lands", function(){
-    plane.land(airport)
-    expect(plane.airport).toEqual(airport)
+    plane.land(Gatwick)
+    expect(plane.airport).toEqual(Gatwick)
   })
+
 });
